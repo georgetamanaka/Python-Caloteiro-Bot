c7fe2a91ded49e388bfd09f9631a3c3e71726360
georgetamanaka <georgetamanaka@gmail.com>
2018-04-24 17:46:31 -0300

    Data base working for loans

--- bot.py
+++ bot.py
@@ -13,36 +13,65 @@ def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Olá, eu sou o caloteiroBot", reply_markup=reply_markup)
 
 def loan(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="Quem emprestou o dinheiro? 💸", reply_markup=ForceReply(True, True))
-
-def print_info(bot, update):
-#	text = update.message.text
-#	items = db.get_items()
-#	db.add_item(text)
-#	items = db.get_items()
 	db = DBHelper()
 	db.setup()
-	db.add_item(update.message.chat_id, "@george", "@ana", 4.50);
-	db.get_all()
 
-	print ("upadate_id: " + str(update.update_id))	
-	print ("chat_id: " + str(update.message.chat_id))
-	print ("username: " + str(update.message.chat.username))
-	print ("first_name: " + str(update.message.chat.first_name))
-	print ("last_name: " + str(update.message.chat.last_name))
+	chat = db.search_chat((int(update.message.chat_id), )) 
 
-	#db.get_all()
+	if(chat == None):
+		arg1 = (int(update.message.chat_id), 0)
+		db.insert_chat(arg1)
+	
+	db.update_chat_state((0, int(update.message.chat_id)))
+	bot.send_message(chat_id=update.message.chat_id, text="Quem emprestou? 💸", reply_markup=ForceReply(True, True))
 
-	bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+def payment(bot, update):
+	arg1 = (int(update.message.chat_id), )
+	chat = db.search_chat(arg1) 
+
+	if(chat == None):
+		arg1 = (int(update.message.chat_id), 0)
+		db.insert_chat(arg1)
+
+	#bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+def message(bot, update):
+	db = DBHelper()
+	db.setup()
+
+	chat = db.search_chat((int(update.message.chat_id), )) 
+	print("chaaat: " + str(chat))
+
+	if(chat == None):
+		start(bot, update)
+	else:
+		state = int(chat[1])
+
+		if(state == 0):
+			db.update_chat_creditor((update.message.text, update.message.chat_id))
+			db.update_chat_state((1, int(update.message.chat_id)))
+			bot.send_message(chat_id=update.message.chat_id, text="Quem recebeu? 💸", reply_markup=ForceReply(True, True))
+		elif(state == 1):
+			db.update_chat_debtor((update.message.text, update.message.chat_id))
+			db.update_chat_state((2, int(update.message.chat_id)))
+			bot.send_message(chat_id=update.message.chat_id, text="Qual o valor? 💸", reply_markup=ForceReply(True, True))
+		elif(state == 2):
+			value = float(update.message.text)
+			db.update_balance((chat[0], chat[2], chat[3], value))
+			message = "Emprestimo registrado!\nCredor: " + str(chat[2]) + "\nCaloteiro: " + str(chat[3]) + "\nValor: " + str(value)
+			bot.send_message(chat_id=update.message.chat_id, text=message)
 
 def main():
 	updater = Updater(token='517500543:AAGRhhagJCuwESGJI1Ye2iSQb32POHZsnm4')
 	dispatcher = updater.dispatcher
+	
 	logging.basicConfig(level=logging.DEBUG,
 	                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	
 	dispatcher.add_handler(CommandHandler('start', start))
 	dispatcher.add_handler(CommandHandler('emprestimo', loan))
-	dispatcher.add_handler(MessageHandler(Filters.text, print_info))
+	dispatcher.add_handler(CommandHandler('pagamento', payment))
+	dispatcher.add_handler(MessageHandler(Filters.text, message))
 	dispatcher.add_handler(MessageHandler(Filters.command, unknow))
 
 	updater.start_polling()
--- dbhelper.py
+++ dbhelper.py
@@ -8,58 +8,66 @@ class DBHelper:
 
     def setup(self):
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS balance (
-            chat_id varchar(10),
+            chat_id int,
             creditor_id varchar(20),
             debtor_id varchar(20),
             value float
         );""")
 
-    def select_register(self, args):
-        #args = (str(chat_id), creditor_username, debtor_username)
-        self.cursor.execute("SELECT * FROM balance WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS chat_state (
+            chat_id int,
+            state int,
+            creditor_id varchar(20),
+            debtor_id varchar(20)
+        );""")
 
+    def search_chat(self, args):
+        self.cursor.execute("SELECT * FROM chat_state WHERE chat_id = ?", args)
         return self.cursor.fetchone()
 
-    def delete_register(self, args):
-        self.cursor.execute("DELETE * FROM balance WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
-
-    def update_register(self, args):
-        self.cursor.execute("UPDATE balance SET value = ? WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
-
-    def add_register(self, args):
-        #reg = self.select_register(str(chat_id), creditor_username, debtor_username)
-
-        #if (reg == None):
-        #   args = (str(chat_id), creditor_username, debtor_username, round(value, 2))
-        self.cursor.execute("INSERT INTO balance (chat_id, creditor_id, debtor_id, value) VALUES (?, ?, ?, ?)", args)
-        #else:
-        #   self.update_register(args)
-            
+    def remove_chat(self, args):
+        self.cursor.execute("DELETE * FROM chat_state WHERE chat_id = ?", args)
         self.conn.commit()
 
-    def overview_chat(self, chat_id):
-        arg1 = (str(chat_id),)
-        self.cursor.execute("SELECT * FROM balance WHERE chat_id = ?", arg1)
+    def update_chat_state(self, args):
+        self.cursor.execute("UPDATE chat_state SET state = ? WHERE chat_id = ?", args)
+        self.conn.commit()
 
-        #user = ""
+    def update_chat_creditor(self, args):
+        self.cursor.execute("UPDATE chat_state SET creditor_id = ? WHERE chat_id = ?", args)
+        self.conn.commit()
+
+    def update_chat_debtor(self, args):
+        self.cursor.execute("UPDATE chat_state SET debtor_id = ? WHERE chat_id = ?", args)
+        self.conn.commit()
+
+    def insert_chat(self, args):
+        self.cursor.execute("INSERT INTO chat_state (chat_id, state) VALUES (?, ?)", args)
+        self.conn.commit()
+
+    def search_balance(self, args):
+        self.cursor.execute("SELECT * FROM balance WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
+        return self.cursor.fetchone()
+
+    def remove_balance(self, args):
+        self.cursor.execute("DELETE * FROM balance WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
+        self.conn.commit()
+
+    def update_balance(self, args):
+        self.cursor.execute("UPDATE balance SET value = ? WHERE chat_id = ? AND creditor_id = ? AND debtor_id = ?", args)
+        self.conn.commit()
+
+    def insert_balance(self, args):
+        self.cursor.execute("INSERT INTO balance (chat_id, creditor_id, debtor_id, value) VALUES (?, ?, ?, ?)", args)
+        self.conn.commit()
+
+    def overview_balance(self, args):
+        self.cursor.execute("SELECT * FROM balance WHERE chat_id = ?", args)
         vector = self.cursor.fetchall();
-        vector.sort(key=lambda x:x[2])
-
-        #print ("Balanço de dívidas:")
-
-        #i = 0
-
-        #for linha in vector:
-        #   if(user != linha[2]):
-        #       i += 1
-        #       print ("\n" + str(i) + ". " + linha[2] + " deve:")
-        #       user = linha[2]
-        #   
-        #   print ("para " + linha[1] + " " + str(linha[3]))
+        return vector.sort(key=lambda x:x[2])
 
     def get_all(self):
-        self.cursor.execute("SELECT * FROM balance")
+        self.cursor.execute("SELECT * FROM chat_state")
 
         for linha in self.cursor.fetchall():
             print(linha)
-
