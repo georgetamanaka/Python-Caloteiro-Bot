fc77b1a8f66f0e6dd6c97229275858c014db5069
Paulo <pandrecarneiro@gmail.com>
2018-04-30 17:24:20 -0300

    bug fixes

--- ArgumentException.py
+++ ArgumentException.py
@@ -2,7 +2,7 @@
 #checa se o numero digitado eh valido
 def number(val):
 	for i in range(len(val)):
-		if((val[i] < '0') or (val[i] >'9') or (val[i] != '.') or (val[i] != ',')):
+		if(((val[i] < '0') or (val[i] >'9')) and ((val[i] != '.') and (val[i] != ','))):
 			return 0;
 		if(val[i] == ','):
 			val[i] = '.'
@@ -16,26 +16,27 @@ def parser(linha, flag):
 	if(flag == 1): #preciso ler dois usuarios e um numero float
 		for i in range(len(linha)):
 			if (linha[i] == '@'): #andei ate chegar no primeiro usuario
-				while((linha[i] != ' ') and (i < len(linha))):
+				while((i < len(linha)) and (linha[i] != ' ')):
 					if(aux == 0):
 						aux = 1
 					user1 += linha[i]
 					i+=1
-				while((linha[i] != '@') and (i < len(linha))): #retirando o espaco em branco e os lixos de escrita ate chegar no outro @
+				while((i < len(linha)) and (linha[i] != '@')): #retirando o espaco em branco e os lixos de escrita ate chegar no outro @
 					i += 1
-				while((linha[i] != ' ') and (i < len(linha))):
+
+				while((i < len(linha)) and (linha[i] != ' ') ):
 					if(aux == 1):
 						aux = 2
 					user2 += linha[i]
 					i+=1
-				while((linha[i] == ' ') and (i < len(linha))): #retirando o espaco em branco
+				while( (i < len(linha)) and (linha[i] == ' ')): #retirando o espaco em branco
 					i += 1
 				while((i < len(linha))):
 					if(aux == 2):
 						aux = 3
 					val += linha[i]
 					i+=1
-
+				break
 		if((aux == 3) and (number(val) == 1)): #esse aux checa se os 3 campos foram preenchidos de maneira correta
 			print(user1)
 			print(user2)
@@ -46,11 +47,12 @@ def parser(linha, flag):
 	elif(flag ==2): #aqui so tenho que ler um unico usuario
 		for i in range(len(linha)):
 			if (linha[i] == '@'): #andei ate chegar no primeiro usuario
-				while((linha[i] != ' ') and (i < len(linha))):
+				while((i < len(linha)) and (linha[i] != ' ') ):
 					if(aux == 0):
 						aux = 1
 					user1 += linha[i]
 					i+=1
+				break
 		if(aux == 1):
 			return {user1, -1, -1}
 		else:
@@ -58,18 +60,19 @@ def parser(linha, flag):
 	else:
 		for i in range(len(linha)):
 			if (linha[i] == '@'): #andei ate chegar no primeiro usuario
-				while((linha[i] != ' ') and (i < len(linha))):
+				while((i < len(linha)) and (linha[i] != ' ')):
 					if(aux == 0):
 						aux = 1
 					user1 += linha[i]
 					i+=1
-				while((linha[i] != '@') and (i < len(linha))): #retirando o espaco em branco e os lixos de escrita ate chegar no outro @
+				while( (i < len(linha)) and (linha[i] != '@')): #retirando o espaco em branco e os lixos de escrita ate chegar no outro @
 					i += 1
-				while((linha[i] != ' ') and (i < len(linha))):
+				while((i < len(linha)) and (linha[i] != ' ') ):
 					if(aux == 1):
 						aux = 2
 					user2 += linha[i]
 					i+=1
+				break
 		if((aux == 2)): #esse aux checa se os 2 campos foram preenchidos de maneira correta
 			print(user1)
 			print(user2)
